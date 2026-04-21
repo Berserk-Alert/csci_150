ce9f0504fa9cee430310ba7a29eff0971181d43f
Berserk_Alert <djzhoucao33939@gmail.com>
2026-04-20 20:42:13 -0700

    new topic 6 and hw

--- self/practice.asm
+++ self/practice.asm
@@ -1,31 +1,29 @@
-;who:   
-;what:  
-;why:   
-;when:  
-
-global  _start
-section .text
-_start:
-    mov     ecx, 10
-
-    .loop:
-    mov     eax, [temp]
-    mov     eax, (10 - ecx)
-    loop
-
-
-exit: 
-    mov     eax, 1
-    mov     ebx, 0
-    int     0x80
-
-section .bss
-; buffer:     rest    1
+global _start
 
 section .data
+    arr     dd  0,1,2,3,4,5,6,7,8,9   ; 10 doublewords (4 bytes each)
+    arr_sz  equ $ - arr                ; total byte size = 40
 
-temp:       db      '1', 0x0a
-buffer:     db      0, 10
-array:      db      "1,2,3,4,5,6,7,8,9,0"
-endline:    db      10
-array2:     db      'look at that, we have a on overflow'
+section .text
+_start:
+
+    mov eax, 0              ; eax will accumulate the sum
+    mov ecx, arr_sz / 4     ; ecx = number of elements (40/4 = 10)
+    mov esi, [arr]              ; esi = array index
+
+.loop:
+    cmp ecx, 0              ; are there elements left?
+    je  .done
+
+    add eax, esi  ; eax += arr[esi]
+
+    add esi, 4             ; next index
+    dec ecx                 ; one less element
+    jmp .loop
+
+.done:
+    ; eax now holds the sum (0+1+2+...+9 = 45)
+
+    mov ebx, 0
+    mov eax, 1              ; syscall: exit
+    int 0x80
